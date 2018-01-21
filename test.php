18986b84f254d2cee5ca32b463033d7ae6616922
KazutakaIga <kazutaka.iga@gmail.com>
2018-01-22 04:36:48 +0900

    Update test.php

--- test.php
+++ test.php
@@ -2,11 +2,17 @@
 ini_set('display_errors',1);
 require('class/database.php');
 
+$db_hostname = "localhost";
+$database_name = "seal";
+$table_name = "PLAYLIST";
+$username = "root";
+$password = "";
+
 $sql = new database();
 $sql_text = "SELECT * FROM PLAYLIST";
-$sql->exec_sql("localhost",
-               "root",
-               "dile6245",
-               "seal",
+$sql->exec_sql($db_hostname,
+               $username,
+               $password,
+               $database_name,
                $sql_text);
 ?>
